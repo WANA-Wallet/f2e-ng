151e197315b1e6af1bf392048ad9dd35a1c00645
gjlmotea <gjlmotea@gmail.com>
2022-07-02 17:51:17 +0800

    first commit

---
+++ src/app/layout/main/main.component.ts
@@ -0,0 +1,23 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+declare var scriptInit: any;
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('start')
+    new scriptInit();
+    console.log('end')
+  }
+
+}
